6a92b5fc031de315ef7c857d030b930d2484236a
kushnerovM <Rojers1941@yandex.by>
2022-12-11 20:25:17 +0300

    word pair data structure added

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 //elements
+var pair = ["English", "Английский"];
 var wordTextbox = document.querySelector(".word-textbox");
 var card = document.querySelector(".card");
+var englishCaption = document.querySelector(".english-caption");
+var translatedCaption = document.querySelector(".translated-caption");
 //variables
 //event listeners
 wordTextbox.addEventListener("keyup", function () {
-    if (wordTextbox.value === "Английский") {
+    if (wordTextbox.value === pair[1]) {
         document.querySelector(".front").style.transform = "perspective(500px) rotateY(180deg)";
         document.querySelector(".back").style.transform = "perspective(500px) rotateY(360deg)";
     }
 });
 //functions
-console.log("Hello, world!");
+//executable code
+englishCaption.textContent = pair[0];
+translatedCaption.textContent = pair[1];
--- app.ts
+++ app.ts
@@ -1,14 +1,16 @@
 //elements
-
+let pair: string [] = ["English","Английский"]
 const wordTextbox = document.querySelector(".word-textbox") as HTMLInputElement;
 const card = document.querySelector(".card");
+const englishCaption = document.querySelector(".english-caption")!;
+const translatedCaption = document.querySelector(".translated-caption")!;
 //variables
 
 
 
 //event listeners
 wordTextbox.addEventListener("keyup",()=>{
-    if(wordTextbox.value==="Английский"){
+    if(wordTextbox.value===pair[1]){
         document.querySelector<HTMLElement>(".front")!.style.transform = "perspective(500px) rotateY(180deg)";
         document.querySelector<HTMLElement>(".back")!.style.transform = "perspective(500px) rotateY(360deg)";
     }   
@@ -17,3 +19,7 @@ wordTextbox.addEventListener("keyup",()=>{
 
 
 //functions
+
+//executable code
+englishCaption.textContent=pair[0];
+translatedCaption.textContent=pair[1];
