f06ef4bc58ca1f4a289a9d39276a9d2a3dda4b96
kushnerovM <Rojers1941@yandex.by>
2022-12-14 20:11:02 +0300

    bind check-feature to button

--- app.js
+++ app.js
@@ -4,18 +4,20 @@ var card = document.querySelector(".card");
 var englishCaption = document.querySelector(".english-caption");
 var translatedCaption = document.querySelector(".translated-caption");
 var nextButton = document.querySelector(".next-button");
-var resultContainer = document.querySelector(".result .container");
+var checkButton = document.querySelector("#check-btn");
+var giveUpButton = document.querySelector("#give-up-btn");
+var resultContainer = document.querySelector(".result");
 //variables
 var pair = ["English", "Английский"];
 //event listeners
-wordTextbox.addEventListener("keyup", function () {
-    if (wordTextbox.value === pair[1]) {
-        document.querySelector(".result").style.height = "50%";
-        resultContainer.style.visibility = "visible";
-        nextButton.style.opacity = "1";
-        // document.querySelector<HTMLElement>(".front")!.style.boxShadow="0 0 2px 5px #04f06a";
+/*wordTextbox.addEventListener("keyup",()=>{
+    if(wordTextbox.value===pair[1]){
+        document.querySelector<HTMLElement>(".result")!.style.height="50%";
+        resultContainer.style.visibility="visible";
+        (nextButton as HTMLElement).style.opacity="1";
     }
-});
+});*/
+checkButton.addEventListener("click", check);
 nextButton.addEventListener("click", function () {
     rotate(document.querySelector(".front"), 180);
     rotate(document.querySelector(".back"), 360);
@@ -23,6 +25,12 @@ nextButton.addEventListener("click", function () {
 //functions
 function rotate(elem, value) {
     elem.style.transform = "perspective(500px) rotateY(".concat(value, "deg)");
+}
+function check() {
+    resultContainer.style.backgroundColor = wordTextbox.value === pair[1] ? "#1bcd68" : "#ee6352";
+    resultContainer.style.height = "50%";
+    document.querySelector(".result .container").style.visibility = "visible";
+    nextButton.style.opacity = "1";
 }
 //executable code
 englishCaption.textContent = pair[0];
--- app.ts
+++ app.ts
@@ -4,20 +4,23 @@ const card = document.querySelector(".card");
 const englishCaption = document.querySelector(".english-caption")!;
 const translatedCaption = document.querySelector(".translated-caption")!;
 const nextButton = document.querySelector(".next-button")!;
-const resultContainer = document.querySelector(".result .container") as HTMLElement;
+const checkButton = document.querySelector("#check-btn")!;
+const giveUpButton = document.querySelector("#give-up-btn")!;
+const resultContainer = document.querySelector(".result") as HTMLElement;
 
 //variables
 let pair: string [] = ["English","Английский"];
 
 //event listeners
-wordTextbox.addEventListener("keyup",()=>{
+/*wordTextbox.addEventListener("keyup",()=>{
     if(wordTextbox.value===pair[1]){
         document.querySelector<HTMLElement>(".result")!.style.height="50%";
         resultContainer.style.visibility="visible";
         (nextButton as HTMLElement).style.opacity="1";
-       // document.querySelector<HTMLElement>(".front")!.style.boxShadow="0 0 2px 5px #04f06a";
     }   
-});
+});*/
+
+checkButton.addEventListener("click",check);
 
 nextButton.addEventListener("click",()=>{
     rotate(document.querySelector(".front")!,180);
@@ -28,6 +31,13 @@ nextButton.addEventListener("click",()=>{
 
 function rotate (elem :HTMLElement,value: number): void {
     elem.style.transform = `perspective(500px) rotateY(${value}deg)`
+}
+
+function check () : void{
+        resultContainer.style.backgroundColor=wordTextbox.value===pair[1]?"#1bcd68":"#ee6352";
+        resultContainer.style.height="50%";
+        (document.querySelector(".result .container") as HTMLElement).style.visibility="visible";
+        (nextButton as HTMLElement).style.opacity="1";
 }
 
 //executable code
